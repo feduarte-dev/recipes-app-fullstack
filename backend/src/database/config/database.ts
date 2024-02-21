cbb3b8d55527b20b66df262f5a9ec4c317283932
Felipe Duarte <feduarte.dev@gmail.com>
2024-02-20 22:29:31 -0300

    test3

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,4 +1,5 @@
 import { Dialect, Options } from 'sequelize';
+import * as pg from 'pg';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
@@ -7,6 +8,7 @@ const config: Options = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: process.env.DB_DIALECT as Dialect || 'mysql',
+  dialectModule: pg,
   logging: false,
 };
 
