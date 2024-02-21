31116f5a2f0461062c617706cdf7f2e43abfec7c
Felipe Duarte <feduarte.dev@gmail.com>
2024-02-20 22:23:45 -0300

    test3

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -7,9 +7,6 @@ const config: Options = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: process.env.DB_DIALECT as Dialect || 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
   logging: false,
 };
 
